df4c69377537f14e3fa62417675cb75cd499a5ed
JediQuijada04 <156287282+JediQuijada04@users.noreply.github.com>
2024-07-24 02:02:35 +0800

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,24 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Book extends Model
 {
-    use HasFactory,SoftDeletes;
-    protected $fillable =[
-        'id',
-        'section_id',
+    use HasFactory;
+
+    protected $fillable = [
+        'isbn',
         'title',
+        'author',
         'description',
-        'book_file_path',
-        'status'
+        'date_published',
     ];
-    public function section()
-    {
-        return $this->belongsTo(Sections::class,'section_id');
-    }
-    public function ratings()
-    {
-        return $this->hasMany(Ratings::class);
-    }
 }
